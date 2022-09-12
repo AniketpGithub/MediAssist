4388c408d3cf8a5c6dd1012b90cb955497350e95
Aniket <pashteaniket8@gmail.com>
2022-09-12 19:19:21 +0530

    descriptive comment for code changes

---
+++ src/test/kotlin/com/example/MedicalAssistance/controller/BookAppointmentControllerTest.kt
@@ -0,0 +1,5 @@
+package com.example.MedicalAssistance.controller
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class BookAppointmentControllerTest
---
+++ src/test/kotlin/com/example/MedicalAssistance/service/BookAppointmentServiceTest.kt
@@ -0,0 +1,5 @@
+package com.example.MedicalAssistance.service
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class BookAppointmentServiceTest
